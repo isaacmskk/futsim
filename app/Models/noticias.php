8832c57515c33fc1709f0b7c417b928bdaa85e1a
isaac <isaac@prueba.com>
2024-03-11 19:59:32 +0100

    noticias se añade correctamente la foto y visualizar. con todos los cambios de controller modelo bbdd etc...

--- app/Models/noticias.php
+++ app/Models/noticias.php
@@ -4,17 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class noticias extends Model
+class noticias extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
     protected $fillable = [
         'id',
         'titulo',
         'subtitulo',
         'contenido',
         'publicado',
-        'foto'
     ];
-
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('images/jugadores')
+            ->useFallbackUrl('/images/placeholder.jpg')
+            ->useFallbackPath(public_path('/images/placeholder.jpg'));
+    }
 }
--- database/migrations/2024_02_26_182605_create_noticias_table.php
+++ database/migrations/2024_02_26_182605_create_noticias_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string("subtitulo");
             $table->string("contenido");
             $table->dateTime("publicado");
-            $table->string("foto");
             $table->timestamps();
         });
     }
