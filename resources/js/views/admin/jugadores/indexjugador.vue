c557e8f9a51b9f86dd9e694c651b2f664848f1b5
Unai <unai@prueba.com>
2024-03-12 16:53:56 +0100

    Modificación de diseño para la visión de los jugadores con sus cartas, y añadir boton para seleccionar los jugadores

--- resources/js/views/admin/jugadores/indexjugador.vue
+++ resources/js/views/admin/jugadores/indexjugador.vue
@@ -22,7 +22,7 @@
                             </tr>
                         </thead> -->
                     <tbody class="row">
-                        <tr v-for="(jugador, index) in jugadores" class="card col-4 cartJugadores">
+                        <tr v-for="(jugador, index) in jugadores" class="card col-4 cartJugadores text-center">
                             <!-- <td class="text-center">{{ jugador.id }}</td>
                                 <td>{{ jugador.nombre }}</td>
                                 <td>{{ jugador.apellido }}</td>
@@ -31,8 +31,11 @@
                                 <td>{{ jugador.valoracion }}</td> -->
 
                             <!-- <div class="col-4"> -->
-                                <td class="">
-                                    <img :src="`${jugador.media[0]?.original_url}`" alt="Imagen Jugador">
+                                <td class="p-2">
+                                    <img :src="`${jugador.media[0]?.original_url}`" alt="Imagen Jugador" class="imgJugador">
+                                </td>
+                                <td class="text-center">
+                                    <button class="btn btn-success" @click="">Seleccionar</button> 
                                 </td>
                             <!-- </div> -->
                         </tr>
@@ -60,96 +63,6 @@ onMounted(() => {
         })
 });
 
-const deleteJugador = (id, index) => {
-    swal({
-        title: 'Quieres eliminar el jugador?',
-        text: 'Esta acción no es reversible!',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Sí, eliminar',
-        confirmButtonColor: '#ef4444',
-        timer: 20000,
-        timerProgressBar: true,
-        reverseButtons: true
-    })
-        .then(result => {
-            axios.delete('/api/jugadores/' + id)
-                .then(response => {
-                    jugadores.value.splice(index, 1)
-                    swal({
-                        icon: 'success',
-                        title: 'Jugador eliminado correctamente'
-                    })
-
-                }).catch(error => {
-                    swal({
-                        icon: 'error',
-                        title: 'No se ha podido eliminar el jugador'
-                    })
-
-                });
-
-        })
-}
-
-const updateJugador = (id, index) => {
-
-    const currentData = jugadores.value[index];
-
-    swal({
-        title: 'Editar publicación',
-        html:
-            '<input id="swal-input1" class="swal2-input" placeholder="" value="' + currentData.id + '">' +
-            '<input id="swal-input2" class="swal2-input" placeholder="" value="' + currentData.nombre + '">' +
-            '<input id="swal-input3" class="swal2-input" placeholder="" value="' + currentData.apellido + '">' +
-            '<input id="swal-input4" class="swal2-input" placeholder="" value="' + currentData.posicion + '">' +
-            '<input id="swal-input5" class="swal2-input" placeholder="" value="' + currentData.nacionalidad + '">' +
-            '<input id="swal-input6" class="swal2-input" placeholder="" value="' + currentData.valoracion + '">' +
-            '<input id="swal-input7" class="swal2-input" placeholder="" value="' + currentData.carta + '">',
-
-        focusConfirm: false,
-        preConfirm: () => {
-            return [
-                document.getElementById('swal-input1').value,
-                document.getElementById('swal-input2').value,
-                document.getElementById('swal-input3').value,
-                document.getElementById('swal-input4').value,
-                document.getElementById('swal-input5').value,
-                document.getElementById('swal-input6').value,
-                document.getElementById('swal-input7').value
-
-
-            ]
-        }
-    }).then(result => {
-        if (result.isConfirmed) {
-            const updatedData = {
-                id: result.value[0],
-                nombre: result.value[1],
-                apellido: result.value[2],
-                posicion: result.value[3],
-                nacionalidad: result.value[4],
-                valoracion: result.value[5],
-                carta: result.value[6]
-
-            };
-
-            axios.put('/api/jugadores/update/' + id, updatedData)
-                .then(response => {
-                    jugadores.value.splice(index, 1, updatedData);
-                    swal({
-                        icon: 'success',
-                        title: 'Jugador actualizado correctamente'
-                    });
-                }).catch(error => {
-                    swal({
-                        icon: 'error',
-                        title: 'No se ha podido actualizar el jugador'
-                    });
-                });
-        }
-    });
-};
 
 </script>
 
