32267f46faddfe5fe7235b5068d3366a8f673459
isaac <isaac@prueba.com>
2024-03-15 16:59:48 +0100

    commit añadido css de noticiaindividual e intentos de recoger imagen en vue de indexnoticiaindividual

--- resources/js/views/admin/noticias/indexnoticiaindividual.vue
+++ resources/js/views/admin/noticias/indexnoticiaindividual.vue
@@ -4,57 +4,40 @@
             <div class="card">
                 <div class="card-body">
                     <div class="d-flex justify-content-between pb-2 mb-2">
-                        <h5 class="card-title">Todas las noticias</h5>
+                        <h5 class="card-title">{{ noticia.titulo }}</h5>
                     </div>
-
-                    <table class="table table-hover table-sm">
-                        <thead class="bg-dark text-light">
-                            <tr>
-                                <th width="50" class="text-center">#</th>
-                                <th>Titulo</th>
-                                <th>Subtitulo</th>
-                                <th>Contenido</th>
-                                <th>Publicado</th>
-                                <th>foto</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <tr v-for="(noticia, index) in noticias" :key="noticia.id">
-                                <td>{{ noticia.id }}</td>
-                                <td>{{ noticia.titulo }}</td>
-                                <td>{{ noticia.subtitulo }}</td>
-                                <td>{{ noticia.contenido }}</td>
-                                <td>{{ noticia.publicado }}</td>
-                                <td>{{ noticia.foto }}</td>
-
-                                <td class="text-center">
-                                    <button class="btn btn-success"
-                                        @click="mostrarFormularioComentario(noticia.id)">Comentar</button>
-
-                                    <table v-if="comentariosPorNoticia[noticia.id]" class="table table-hover table-sm">
-                                        <thead class="bg-dark text-light">
-                                            <tr>
-                                                <th width="50" class="text-center">#</th>
-                                                <th>Comentario</th>
-                                                <th>ID Usuario</th>
-                                                <th>Fecha y Hora</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr v-for="(comentario, index) in comentariosPorNoticia[noticia.id]"
-                                                :key="comentario.id">
-                                                <td class="text-center">{{ comentario.id }}</td>
-                                                <td>{{ comentario.comentario }}</td>
-                                                <td>{{ comentario.id_usuario }}</td>
-                                                <td>{{ comentario.time }}</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <img :src="`${noticia.media[0]?.original_url}`" alt="Imagen Noticia" class="img-fluid">
+                        </div>
+                        <div class="col-lg-6">
+                            <p>{{ noticia.subtitulo }}</p>
+                            <p>{{ noticia.contenido }}</p>
+                            <p>{{ noticia.publicado }}</p>
+                            
+                            <button class="btn btn-success" @click="mostrarFormularioComentario(noticia.id)">Comentar</button>
+                            
+                            <table v-if="comentariosPorNoticia[noticia.id]" class="table table-hover table-sm">
+                                <thead class="bg-dark text-light">
+                                    <tr>
+                                        <th width="50" class="text-center">#</th>
+                                        <th>Comentario</th>
+                                        <th>ID Usuario</th>
+                                        <th>Fecha y Hora</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="(comentario, index) in comentariosPorNoticia[noticia.id]" :key="comentario.id">
+                                        <td class="text-center">{{ comentario.id }}</td>
+                                        <td>{{ comentario.comentario }}</td>
+                                        <td>{{ comentario.id_usuario }}</td>
+                                        <td>{{ comentario.time }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -69,11 +52,13 @@ import { useRoute } from 'vue-router';
 const swal = inject('$swal');
 const comentariosPorNoticia = ref({});
 const route = useRoute();
-const noticias = ref([]);
+const noticia = ref(null);
+
 onMounted(() => {
     axios.get('/api/noticias/' + route.params.id)
         .then(response => {
-            noticias.value = [response.data]; // Tratar como un array
+            noticia.value = response.data;
+            console.log(response.data);
         })
         .catch(error => {
             console.error('Error fetching news data:', error);
@@ -85,7 +70,6 @@ onMounted(() => {
             comentariosPorNoticia.value = groupComentariosPorNoticia(response.data);
         });
 });
-
 // Función para organizar comentarios por noticia
 const groupComentariosPorNoticia = (comentarios) => {
     const groupedComentarios = {};
@@ -102,13 +86,10 @@ const groupComentariosPorNoticia = (comentarios) => {
 const mostrarFormularioComentario = (idNoticia) => {
     swal({
         title: 'Agregar Comentario',
-        html:
-            '<input id="swal-comentario" class="swal2-input" placeholder="Comentario">',
+        html: '<input id="swal-comentario" class="swal2-input" placeholder="Comentario">',
         focusConfirm: false,
         preConfirm: () => {
-            return {
-                comentario: document.getElementById('swal-comentario').value
-            };
+            return { comentario: document.getElementById('swal-comentario').value };
         }
     }).then(result => {
         if (result.isConfirmed) {
@@ -124,16 +105,12 @@ const mostrarFormularioComentario = (idNoticia) => {
 const crearComentario = (nuevoComentario) => {
     axios.post('/api/comentarios', nuevoComentario)
         .then(response => {
-            // Agregar el nuevo comentario a la lista local de comentarios
             if (!comentariosPorNoticia.value[nuevoComentario.id_noticia]) {
                 comentariosPorNoticia.value[nuevoComentario.id_noticia] = [];
             }
 
             comentariosPorNoticia.value[nuevoComentario.id_noticia].push(response.data.data);
-            swal({
-                icon: 'success',
-                title: 'Comentado correctamente :-)'
-            });
+            swal({ icon: 'success', title: 'Comentado correctamente :-)' });
         })
         .catch(error => {
             strSuccess.value = "";
