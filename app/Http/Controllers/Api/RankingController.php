a4c9d39977cc195e3cb9b6ad32c5a0f62f627e89
Unai <unai@prueba.com>
2024-04-04 17:39:46 +0200

    Generamos la funcion en el controller para recibir el nombre de usaurio y sus puntos obtenidos golbalmente para realizar el ranikg, al igual que creamos la vista de ranking  con la tabla donde iran los resultados obtenidos por el controller y la funcion donde llamamos a la api

--- app/Http/Controllers/Api/RankingController.php
+++ app/Http/Controllers/Api/RankingController.php
@@ -1,11 +1,30 @@
 <?php
 
-namespace App\Http\Controllers\api;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\usuario_partido;
+use Illuminate\Support\Facades\DB;
 
 class RankingController extends Controller
 {
+    public function index()
+    {
+        // Obtener los usuarios con sus puntos totales
+        $usuarios = DB::table('usuario_partidos')
+                        ->select('id_usuario', DB::raw('SUM(resultado) as total_puntos'))
+                        ->groupBy('id_usuario')
+                        ->orderByDesc('total_puntos')
+                        ->get();
 
+        // Añadir la posición de cada usuario
+        $posicion = 1;
+        foreach ($usuarios as $usuario) {
+            $usuario->posicion = $posicion++;
+        }
+
+        return response()->json($usuarios);
+    }
 }
+
--- resources/js/views/admin/ranking/indexranking.vue
+++ resources/js/views/admin/ranking/indexranking.vue
@@ -1,192 +1,56 @@
 <template>
     <div class="grid">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between pb-2 mb-2">
-                        <h5 class="card-title">Todas las noticias</h5>
-                        <div>
-                            <router-link :to="{ name: 'futsimvistas.createnoticia' }" class="botonGeneral">Nueva
-                                Noticia</router-link>
-                        </div>
-
-                    </div>
-
-                    <table class="table table-hover table-sm">
-                        <thead class="bg-dark text-light">
-                            <tr>
-                                <th width="50" class="text-center">#</th>
-                                <th>Titulo</th>
-                                <th>Subtitulo</th>
-                                <th>Contenido</th>
-                                <th>Publicado</th>
-                                <th>foto</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(noticia, index) in noticias" :key="noticia.id">
-                                <td class="text-center">{{ noticia.id }}</td>
-                                <td>{{ noticia.titulo }}</td>
-                                <td>{{ noticia.subtitulo }}</td>
-                                <td>{{ noticia.contenido }}</td>
-                                <td>{{ noticia.publicado }}</td>
-                                <td>            
-                                    <img :src="`${noticia.media[0]?.original_url}`" alt="Imagen Noticia" class="imgJugador">
-                                </td>
-                                <td class="text-center">
-                                    <button class="btn btn-danger"
-                                        @click="deleteNoticia(noticia.id, index)">Delete</button>
-                                    <button class="btn btn-success"
-                                        @click="mostrarFormularioComentario(noticia.id)">Comentar</button>
-                                    
-                                        <button class="btn btn-success" @click="detallenoticia(noticia.id)">Ver noticia</button>
-                                    <!-- Mostrar comentarios solo para la noticia actual -->
-                                    <table v-if="comentariosPorNoticia[noticia.id]" class="table table-hover table-sm">
-                                        <thead class="bg-dark text-light">
-                                            <tr>
-                                                <th width="50" class="text-center">#</th>
-                                                <th>Comentario</th>
-                                                <th>ID Usuario</th>
-                                                <th>Fecha y Hora</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr v-for="(comentario, index) in comentariosPorNoticia[noticia.id]"
-                                                :key="comentario.id">
-                                                <td class="text-center">{{ comentario.id }}</td>
-                                                <td>{{ comentario.comentario }}</td>
-                                                <td>{{ comentario.id_usuario }}</td>
-                                                <td>{{ comentario.time }}</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <div class="d-flex justify-content-between pb-2 mb-2">
+              <h5 class="card-title">RANKING</h5>
             </div>
+  
+            <table class="table table-hover table-sm">
+              <thead class="bg-dark text-light">
+                <tr>
+                  <th>Posicion</th>
+                  <th>Username</th>
+                  <th>Puntos</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(usuario, index) in ranking" :key="usuario.id_usuario">
+                  <td class="text-center">{{ usuario.posicion }}</td>
+                  <td class="text-center">{{ usuario.nombre }}</td>
+                  <td class="text-center">{{ usuario.total_puntos }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
+      </div>
     </div>
-</template>
-
-<script setup>
-import axios from "axios";
-import { ref, onMounted, inject } from "vue"
-import { useRouter } from 'vue-router';
-
-const noticias = ref();
-const swal = inject('$swal');
-const comentariosPorNoticia = ref({});
-const router = useRouter();
-onMounted(() => {
-    axios.get('/api/noticias')
-        .then(response => {
-            noticias.value = response.data;
-        });
-
-        axios.get('/api/comentarios')
-        .then(response => {
-            // Organizar comentarios por noticia
-            comentariosPorNoticia.value = groupComentariosPorNoticia(response.data);
-        });
-});
-
-
-
-// Función para organizar comentarios por noticia
-const groupComentariosPorNoticia = (comentarios) => {
-    const groupedComentarios = {};
-    comentarios.forEach((comentario) => {
-        const idNoticia = comentario.id_noticia;
-        if (!groupedComentarios[idNoticia]) {
-            groupedComentarios[idNoticia] = [];
-        }
-        groupedComentarios[idNoticia].push(comentario);
-    });
-    return groupedComentarios;
-};
-
-const deleteNoticia = (id, index) => {
-    swal({
-        title: 'Quieres eliminar la noticia?',
-        text: 'Esta acción no es reversible!',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Sí, eliminar',
-        confirmButtonColor: '#ef4444',
-        timer: 20000,
-        timerProgressBar: true,
-        reverseButtons: true
-    })
-        .then(result => {
-            axios.delete('/api/noticias/' + id)
-                .then(response => {
-                    noticias.value.splice(index, 1)
-                    swal({
-                        icon: 'success',
-                        title: 'Noticia eliminada correctamente'
-                    })
-
-                }).catch(error => {
-                    swal({
-                        icon: 'error',
-                        title: 'No se ha podido eliminar la noticia'
-                    })
-
-                });
-
-        })
-}
-const mostrarFormularioComentario = (idNoticia) => {
-    swal({
-        title: 'Agregar Comentario',
-        html:
-            '<input id="swal-comentario" class="swal2-input" placeholder="Comentario">',
-        focusConfirm: false,
-        preConfirm: () => {
-            return {
-                comentario: document.getElementById('swal-comentario').value
-            };
-        }
-    }).then(result => {
-        if (result.isConfirmed) {
-            const nuevoComentario = {
-                id_noticia: idNoticia,
-                comentario: result.value.comentario
-            };
-            crearComentario(nuevoComentario);
-        }
-    });
-};
-const crearComentario = (nuevoComentario) => {
-    axios.post('/api/comentarios', nuevoComentario)
-        .then(response => {
-
-            // Agregar el nuevo comentario a la lista local de comentarios
-            if (!comentariosPorNoticia.value[nuevoComentario.id_noticia]) {
-                comentariosPorNoticia.value[nuevoComentario.id_noticia] = [];
-            }
-            
-            comentariosPorNoticia.value[nuevoComentario.id_noticia].push(response.data.data);
-            // console.log(response.data);
-            // console.log( comentariosPorNoticia.value[nuevoComentario.id_noticia]);
-            swal({
-                    icon: 'success',
-                    title: 'Comentado correctamente :-)'
-                });
-        })
-        .catch(error => {
-            strSuccess.value = "";
-            strError.value = error.response.data.message;
-        });
-};
-const detallenoticia = (idNoticia) => {
-    // Use router.push to navigate to the individual news page
-    console.log(idNoticia);
-    router.push({ name: 'futsimvistas.indexnoticiaindividual', params: { id: idNoticia } });
-}
-
-</script>
-
-<style></style>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
+  
+  const ranking = ref([]);
+  
+  onMounted(() => {
+    cargarRanking();
+  });
+  
+  const cargarRanking = () => {
+    axios.get('/api/ranking')
+      .then(response => {
+        ranking.value = response.data;
+      })
+      .catch(error => {
+        console.error('Error al cargar el ranking:', error);
+      });
+  };
+  </script>
+  
+  <style>
+  /* Estilos CSS según sea necesario */
+  </style>
+  
