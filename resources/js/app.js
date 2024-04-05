a279494cd27807537d789613b0c7ef9e6045fa50
Unai <unai@prueba.com>
2024-04-05 17:55:28 +0200

    Cambios de diseño para la pagina home

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,7 +27,7 @@ import PrimeVue from "primevue/config";
 //import Card from 'primevue/card';
 //import Chart from 'primevue/chart';
 //import CascadeSelect from 'primevue/cascadeselect';
-//import Carousel from 'primevue/carousel';
+import Carousel from 'primevue/carousel';
 //import Checkbox from 'primevue/checkbox';
 //import Chip from 'primevue/chip';
 //import Chips from 'primevue/chips';
