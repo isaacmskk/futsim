09db1975b741dc3e82af82c93b75713af4bd6d91
isaac <isaac@prueba.com>
2024-05-10 17:26:12 +0200

    seeder creado 4 jugadores con imagenes

--- database/seeders/JugadoresTableSeeder.php
+++ database/seeders/JugadoresTableSeeder.php
@@ -1,41 +1,59 @@
 <?php
 
 namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\jugadores;
-
+use App\Models\Jugadores;
 
 class JugadoresTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         $jugadores =  [
             [
-            'nombre' => 'nombre',
-            'apellido' => 'apellido',
-            'posicion' => 'adada',
-            'nacionalidad' => 'nacionalidad',
-            'valoracion' => 5
+                'nombre' => 'Alphonso',
+                'apellido' => 'Davies',
+                'posicion' => 'LI',
+                'nacionalidad' => 'CND',
+                'valoracion' => 94,
+                'imagen_path' => 'images\davies.png' // Ruta de la imagen
             ],
             [
-                'nombre' => 'aaa',
-                'apellido' => 'apellido',
-                'posicion' => 'posicion',
-                'nacionalidad' => 'nacionalidad',
-                'valoracion' => 4
+                'nombre' => 'Merle',
+                'apellido' => 'Frohms',
+                'posicion' => 'POR',
+                'nacionalidad' => 'ALE',
+                'valoracion' => 90,
+                'imagen_path' => 'images\Frohms.png' // Ruta de la imagen
+            ],
+            [
+                'nombre' => 'Ngolo',
+                'apellido' => 'Kante',
+                'posicion' => 'MCD',
+                'nacionalidad' => 'FR',
+                'valoracion' => 94,
+                'imagen_path' => 'images\kante.png' // Ruta de la imagen
+            ],
+            [
+                'nombre' => 'Florian',
+                'apellido' => 'Wirtz',
+                'posicion' => 'MCO',
+                'nacionalidad' => 'ALE',
+                'valoracion' => 96,
+                'imagen_path' => 'images\wirtz.png' // Ruta de la imagen
             ]
         ];
 
-
         foreach ($jugadores as $jugador) {
-            jugadores::create($jugador);
+            $nuevoJugador = Jugadores::create([
+                'nombre' => $jugador['nombre'],
+                'apellido' => $jugador['apellido'],
+                'posicion' => $jugador['posicion'],
+                'nacionalidad' => $jugador['nacionalidad'],
+                'valoracion' => $jugador['valoracion'],
+            ]);
+
+            // Agregar imagen a la colección de medios del jugador
+            $nuevoJugador->addMedia(public_path($jugador['imagen_path']))->preservingOriginal()->toMediaCollection('photo');
         }
     }
 }
